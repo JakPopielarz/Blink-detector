d0cd1ae9ef49d027c310049da8d8f2232c1e5216
JakPopielarz <popielarzjakub7@gmail.com>
2021-11-07 19:57:46 +0100

    Key bind choosing popup and mouse button binding

--- receiver_window.py
+++ receiver_window.py
@@ -5,18 +5,29 @@ import numpy as np
 import matplotlib
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import PySimpleGUI as sg
-from  pynput.keyboard import Key, Controller
+from numpy.typing import _128Bit
+from  pynput.keyboard import Key, Listener
+from pynput.keyboard import Controller as KeyboardController
+from pynput.mouse import Button
+from pynput.mouse import Controller as MouseController
 matplotlib.use('TkAgg')
 
 
-keyboard = Controller()
+bind_keyboard = True
+keyboard = KeyboardController()
+key_bind = Key.enter
+
+mouse = MouseController()
+mouse_bind = Button.left
 
 sg.theme('DefaultNoMoreNagging')
 inputs_layout = [[sg.Text("COM port", size=15), sg.Input("COM6", key="-PORT_INPUT-", size=5, enable_events=True)],
     [sg.Text("Threshold", size=15), sg.Input(500, key="-THRESHOLD_INPUT-", size=5, enable_events=True)],
     [sg.Radio("Single blink to activate", "ACTIVATION_RADIO", key="-1_BLINK_ACTIVATE-", default=True, disabled=True)],
     [sg.Radio("Double blink to activate", "ACTIVATION_RADIO", key="-2_BLINK_ACTIVATE-", default=False, disabled=True)],
-    [sg.Text("Key to press", size=15), sg.Input("Enter", key="-KEY_BIND-", size=5, disabled=True)]]
+    [sg.Text("Key to press", size=10), sg.Input("Enter", key="-CURRENT_KEY-", size=5, enable_events=True, disabled=True), sg.Button("Change key", key="-CHANGE_KEY-", size=10)],
+    [sg.Radio("Key", "BIND_RADIO", key="-KEY_BIND-", default=True, enable_events=True), sg.Radio("LMB", "BIND_RADIO", key="-LMB_BIND-", default=False, enable_events=True), 
+        sg.Radio("RMB", "BIND_RADIO", key="-RMB_BIND-", default=False, enable_events=True)]]
 
 layout = [[sg.Frame("Configuration", inputs_layout, font='Helvetica 18'), sg.Canvas(key='-CANVAS-')],
     [sg.Button('Ok')]]
@@ -37,6 +48,7 @@ def add_plot(window, figure, plot_key):
     return draw_figure(window[plot_key].TKCanvas, figure)
 
 def run_window(window, plotter=None, comm=None):
+    global mouse_bind, bind_keyboard
     while True:
         event, values = window.read(timeout=100)
         if event == sg.WIN_CLOSED or event == 'Exit' or event == 'Ok':
@@ -50,14 +62,24 @@ def run_window(window, plotter=None, comm=None):
         if event == '-THRESHOLD_INPUT-':
             threshold_value = handle_threshold(values['-THRESHOLD_INPUT-'], plotter, window)
 
-        check_receiving(comm, window, threshold_value, values['-KEY_BIND-'])
+        check_receiving(comm, window, threshold_value)
 
         if event == "-PORT_INPUT-":
             comm.stop_receiving()
             comm.port = values["-PORT_INPUT-"]
             comm.start_receiving()
+        elif event == "-CHANGE_KEY-":
+            handle_key_bind(window, "-CURRENT_KEY-")
+        elif event == "-LMB_BIND-":
+            bind_keyboard = False
+            mouse_bind = Button.left
+        elif event == "-RMB_BIND-":
+            bind_keyboard = False
+            mouse_bind = Button.right
+        elif event == "-KEY_BIND-":
+            bind_keyboard = True
 
-def check_receiving(comm, window, threshold_value, key_bind):
+def check_receiving(comm, window, threshold_value):
     if comm is None:
         return
     elif comm.error_in_receiving:
@@ -68,27 +90,31 @@ def check_receiving(comm, window, threshold_value, key_bind):
         block_inputs(window)
         comm.start_receiving()
     else:
-        check_for_blink(comm, threshold_value, key_bind)
+        check_for_blink(comm, threshold_value)
         unblock_inputs(window)
 
 def block_inputs(window):
     window['-THRESHOLD_INPUT-'].update(disabled=True)
     window['-1_BLINK_ACTIVATE-'].update(disabled=True)
     window['-2_BLINK_ACTIVATE-'].update(disabled=True)
-    window['-KEY_BIND-'].update(disabled=True)
+    window['-CHANGE_KEY-'].update(disabled=True)
 
 def unblock_inputs(window):
     window['-THRESHOLD_INPUT-'].update(disabled=False)
     # window['-1_BLINK_ACTIVATE-'].update(disabled=False)
     # window['-2_BLINK_ACTIVATE-'].update(disabled=False)
-    # window['-KEY_BIND-'].update(disabled=False)
+    window['-CHANGE_KEY-'].update(disabled=False)
 
-def check_for_blink(comm, threshold_value, key_bind):
+def check_for_blink(comm, threshold_value):
     sensor_data = comm.get_received()
     if not comm.triggered and sensor_data[-1] >= threshold_value:
         comm.triggered = True
-        keyboard.press(Key.enter)
-        keyboard.release(Key.enter)
+        if bind_keyboard:
+            keyboard.press(key_bind)
+            keyboard.release(key_bind)
+        else:
+            mouse.press(mouse_bind)
+            mouse.release(mouse_bind)
     elif comm.triggered and sensor_data[-1] < threshold_value:
         comm.triggered = False
 
@@ -102,6 +128,23 @@ def handle_threshold(new_val, plotter, window):
         window['-THRESHOLD_INPUT-'].Update('')
     plotter.update_threshold(new_val)
     return new_val
+
+def handle_key_bind(window, text_key):
+    def __rebind(bind):
+        global key_bind
+        key_bind = bind
+        text_val = str(bind)
+        if text_val.startswith('Key'):
+            text_val = text_val[4:]
+        else:
+            text_val = text_val[1:-1]
+        window[text_key].update(text_val)
+
+    # block_inputs(window)
+    key_listener = Listener(on_release=__rebind)
+    key_listener.start()
+    sg.Popup("Press the button you want to be pressed", keep_on_top=True, any_key_closes=True, button_type=sg.POPUP_BUTTONS_NO_BUTTONS)
+    key_listener.stop()
 
 
 if __name__ == "__main__":
