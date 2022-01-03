0fb594b1c3fc6945b47f66faccba3e86efa38609
JakPopielarz <popielarzjakub7@gmail.com>
2022-01-03 23:40:58 +0100

    Arduino code upload

--- Microcontroller/Blink detector.ino
+++ Microcontroller/Blink detector.ino
@@ -5,12 +5,15 @@ void setup(void) {
    Serial.begin(9600);
    pinMode(sensorPin, INPUT);
    pinMode(3, OUTPUT);
-   digitalWrite(3, HIGH);
    analogReference(INTERNAL);
 }
  
 void loop(void) {
+   digitalWrite(3, HIGH);
+   delay(2);
    sensorValue = analogRead(sensorPin);
    Serial.println(sensorValue);
-   delay(10);
+   delay(1);
+   digitalWrite(3, LOW);
+   delay(7);
 }
