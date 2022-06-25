e63a568c0c8e6664231855284e2724a081fe0cae
JakPopielarz <popielarzjakub7@gmail.com>
2022-06-25 15:57:11 +0200

    Initial version of signal analysis functions

--- Microcontroller/Blink detector/Blink detector.ino
+++ Microcontroller/Blink detector/Blink detector.ino
@@ -7,9 +7,9 @@ int sensorPin = A0;
 int sensorValue = 0;
 int ledPin = 3;
 
-// Data array variables
+// Raw data array variables
 int values[VALUE_ARRAY_SIZE]; 
-int lastFilledIndex = 0;
+int lastFilledValueIndex = 0;
 
 // Signal analysis algorithm variables
 double average = 0.0;
@@ -17,17 +17,21 @@ double threshold = 0.0;
 double mean_avg_filter = 0.0;
 double mean_std_filter = 0.0;
 
+// Analysis data array variables
+bool signals[VALUE_ARRAY_SIZE];
+int numberOfNewPoints = 0;
+
 /////////////////////////////////////////////////////////////////////////////////
 //                ARRAY FUNCTIONS
 /////////////////////////////////////////////////////////////////////////////////
 
 /*
-Setup array of values - set every element to -1,
+Setup array of values - set every element to a given value,
 which indicates it hasn't been used yet.
 */
-void setupValueArray(int valueArray[]) {
+void setupArray(int array[], int initValue) {
    for (int i=0; i<VALUE_ARRAY_SIZE; i++) {
-      valueArray[i] = -1;
+      array[i] = -1;
    }
 }
 
@@ -36,17 +40,25 @@ Save a value to the array - if there are still elements previously not used
 override them; if the array is already full - shift elements to the left and
 insert new one on the end.
 */
-void saveValue(int valueArray[], int value) {
-   if (lastFilledIndex < VALUE_ARRAY_SIZE) {
+void appendToSizeLimited(int array[], int size, int value, int lastFilledIndex) {
+   if (lastFilledIndex < size) {
       int index = lastFilledIndex + 1;
-      valueArray[index] = value;
+      array[index] = value;
       lastFilledIndex++;
    } else {
-      for (int i=0; i<VALUE_ARRAY_SIZE-1; i++) {
-         valueArray[i] = valueArray[i+1];
+      for (int i=0; i<size-1; i++) {
+         array[i] = array[i+1];
       }
-      valueArray[VALUE_ARRAY_SIZE-1] = value;
+      array[size-1] = value;
    }
+}
+
+/*
+Save a raw detector value to the array - taking care of size limitations
+*/
+void saveValue(int valueArray[], int value) {
+   appendToSizeLimited(valueArray, VALUE_ARRAY_SIZE, value, lastFilledValueIndex);
+   numberOfNewPoints ++;
 }
 
 /*
@@ -75,9 +87,9 @@ double mean(int array[], int size) {
    for (int i=0; i<size; i++) {
       sum += array[i];
    }
-
    return sum / size;
 }
+
 /*
 Calculate standard deviation of int array
 */
@@ -91,12 +103,54 @@ double std(int array[], int size, double mean) {
    return sum / size;
 }
 
+/*
+Calculate if single point is a peak
+*/
+bool checkDatum(int datum, double mean, double std) {
+   if (abs(datum - mean) > (threshold * std)) {
+   // if point value exceeds the border value ("upwards")
+      if (datum > mean) {
+         return true;
+      }
+   } else {
+      return false;
+   }
+}
+
+/*
+Analyse raw detector values, WITHOUT pre-calculated mean and standard deviation
+*/
+void detect(int array[], int size) {
+   double mean = mean(array, size);
+   double std = std(array, size, mean);
+
+   detect(array, size, mean, std);
+}
+
+/*
+Analyse raw detector values, WITH pre-calculated mean and standard deviation
+*/
+void detect(int array[], int size, double mean, double std) {
+   bool result = false;
+   // there's no need to iterate through the whole data point array - the old points have been checked already
+   // therefore iterate only through the part with new points (added since last analysis)
+   for (int i=size-numberOfNewPoints; i<size; i++) {
+      result = checkDatum(array[i], mean, std);
+      appendToSizeLimited(signals, VALUE_ARRAY_SIZE, result, lastFilledValueIndex);
+   }
+   // checked all new points, so clear the counter
+   numberOfNewPoints = 0;
+}
+
+
+
 /////////////////////////////////////////////////////////////////////////////////
 //                CONTROL FUNCTIONS
 /////////////////////////////////////////////////////////////////////////////////
 
 void setup(void) {
-   setupValueArray(values);
+   setupArray(values, -1);
+   setupArray(signals, 0);
 
    Serial.begin(9600);
    pinMode(sensorPin, INPUT);
